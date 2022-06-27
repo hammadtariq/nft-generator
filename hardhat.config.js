dde1220cf751a2be52581f4f7def5bb8dc2e353f
Hammad Tariq <htariq@nisum.com>
2022-06-27 20:05:18 +0500

    gas reporter plugin added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
+require("hardhat-gas-reporter");
 
 // The next line is part of the sample project, you don't need it in your
 // project. It imports a Hardhat task definition, that can be used for
@@ -10,31 +11,41 @@ require("./tasks/accounts");
 
 // Go to https://www.alchemyapi.io, sign up, create
 // a new App in its dashboard, and replace "KEY" with its key
-const ALCHEMY_API_KEY = process.env.RINKEBY_ALCHEMY_API_KEY;
+const ALCHEMY_API_KEY = process.env.RINKEBY_ALCHEMY_API_KEY || "key";
 
 // Replace this private key with your Ropsten account private key
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Be aware of NEVER putting real Ether into testing accounts
-const PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
-const ETHERSCAN_API = process.env.ETHERSCAN_API;
+const RINKEBY_URL = `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`;
+const PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY || "0xkey";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";
 
 // If you are using MetaMask, be sure to change the chainId to 1337
 module.exports = {
   solidity: "0.8.4",
-  defaultNetwork: "rinkeby",
-  etherscan: {
-    apiKey: {
-      rinkeby: ETHERSCAN_API,
-    },
-  },
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       chainId: 31337,
     },
     rinkeby: {
-      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      url: RINKEBY_URL,
       accounts: [`${PRIVATE_KEY}`],
     },
+  },
+  etherscan: {
+    apiKey: {
+      rinkeby: ETHERSCAN_API_KEY,
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    // token: "MATIC", // to see how much gas will cost on Polygon network
   },
 };
